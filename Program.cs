0a1d4d9ed8a0394f1bdd44cc822464bbbac9712c
Patrice Waechter-Ebling <patdev.qc.ca@outlook.com>
2024-12-18 00:47:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AllocationDynamique1D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] theVecteur;
+
+            theVecteur = new int[2];
+
+            theVecteur[0] = 79;
+            theVecteur[1] = 97;
+            Console.WriteLine($"the Vecteur { GC.GetTotalAllocatedBytes(true)}");
+
+            Console.WriteLine($"the Vecteur[0] { theVecteur[0]}");
+            Console.WriteLine($"the Vecteur[1] { theVecteur[1]}");
+            int a = 0;
+            for (int i=0; i<theVecteur.Length; i++)
+            {
+                Console.WriteLine($"[{i}] = {theVecteur[i]} ");
+            }
+            MaBoucleFor(theVecteur);
+        }
+        static void MaBoucleFor(int[] Patate) 
+        {
+            for (int i = 0; i < Patate.Length; i++) 
+            {
+                Console.WriteLine(Patate[i]);
+            }
+        }
+    }
+}
